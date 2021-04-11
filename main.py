c3bc7a296123d92c0622415526739ab39c72e0ec
Mukesh V <mukeshvinayak2001@gmail.com>
2021-04-11 21:56:52 +0530

    added plots, sampler is biased in semi-major axis

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def random_argp():
 def random_nu():
     return np.random.rand()*180
 
-nwalkers = 100
+nwalkers = 50
 ndim = 6
-MCMC_steps = 100
+MCMC_steps = 500
 
 walkers = []
 for i in range(nwalkers):
@@ -49,7 +49,7 @@ with open('./STARLINK-1401.csv', 'r') as f:
         state_vectors.append([ float(i) for i in row[1:] ])
         epochs.append( int(row[0]) )
 
-def lnprob( param ):
+def lnprob( param, cov ):
     """
     param = [a, inc, raan, ecc, argp, nu]
     """
@@ -78,19 +78,48 @@ def lnprob( param ):
     propagator.compute_necessary_kep(param, b_star=0)
 
     # mean absolute percentange error
-    mape = 0.0
+    error = 0.0
     for index, time in enumerate(epochs):
         if index == 0:
             continue
+        # time = epochs[1]
         gap = time - epochs[0]
         propagated = propagator.propagate(0, gap)[-1]
-        mape += abs((state_vectors[index] - propagated)/state_vectors[index])
+        error += state_vectors[1] - propagated
     
-    mape /= len(epochs)
-    mape = sum(mape) / len(mape)
-    return 1 - mape
+    error /= len(epochs)
+    return -0.5 * np.dot(error, np.linalg.solve(cov, error))
+
+np.random.seed(42)
+means = np.random.rand(ndim)
+cov = 0.5 - np.random.rand(ndim ** 2).reshape((ndim, ndim))
+cov = np.triu(cov)
+cov += cov.T - np.diag(cov.diagonal())
+cov = np.dot(cov, cov)
 
 print(" MCMC running with {} walkers, {} Steps".format(nwalkers, MCMC_steps))
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, threads=8)
-pos, prob, state = sampler.run_mcmc(walkers, MCMC_steps, progress=True)
-print(sum(sampler.chain[-1])/nwalkers)
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=[cov], threads=8)
+sampler.run_mcmc(walkers, MCMC_steps, progress=True)
+samples = sampler.chain[:,0:,:].reshape((-1, ndim))
+
+for i in range(ndim):
+    print(samples[:, i].mean())
+
+xpts = range(0, nwalkers * MCMC_steps)
+fig, axs = plt.subplots(3,2)
+axs[0, 0].plot(xpts, samples[:, 0])
+axs[0, 0].set_title('Semi-Major Axis')
+axs[0, 1].plot(xpts, samples[:, 1])
+axs[0, 1].set_title('Inclination')
+
+axs[1, 0].plot(xpts, samples[:, 2])
+axs[1, 0].set_title('Right Ascension of Ascending Node')
+axs[1, 1].plot(xpts, samples[:, 3])
+axs[1, 1].set_title('Eccentricity')
+
+axs[2, 0].plot(xpts, samples[:, 4])
+axs[2, 0].set_title('Argument of Periapsis')
+axs[2, 1].plot(xpts, samples[:, 5])
+axs[2, 1].set_title('True Anomaly')
+
+plt.show()
